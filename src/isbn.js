72f727fa06a02d61cf93255857b9dda1b42bfabb
Carlos Priego <carlos.priego@ibm.com>
2021-09-01 08:40:59 -0500

    test(🔴): 2 passed, 1 failed, 0 skipped

--- src/isbn.js
+++ src/isbn.js
@@ -1,9 +1,5 @@
 const validateIsbn = (input) => {
-  if (typeof input !== 'string') {
-    throw new Error('Invalid input');
-  }
-
-  if (input.length < 13) {
+  if (typeof input !== 'string' || input.length < 13) {
     throw new Error('Invalid input');
   }
 };
