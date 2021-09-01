5c6cab79f270640568adaca7f6f0866ca89baf9f
Carlos Priego <carlos.priego@ibm.com>
2021-09-01 09:26:56 -0500

    test(🟢): 4 passed, 0 failed, 0 skipped

--- src/isbn.js
+++ src/isbn.js
@@ -7,19 +7,20 @@ const validateInput = (input) => {
 
 const validateIsbn = (input) => {
   validateInput(input);
-  const c = input.split('');
-  let result = 0;
 
-  // 1 x 1st digit, 3 x 2nd digit, 1 x 3rd digit, 3 x 4th digit and sum them
-  for (let index = 0; index < c.length - 1; index += 1) {
-    const element = Number(c[index]);
+  const digits = input.split('');
+  const isbnDigits = digits.slice(0, 11);
+  const verifierDigit = digits.slice(11, 12);
+  let result = 0;
+  for (let index = 0; index < digits.length - 1; index += 1) {
+    const element = Number(digits[index]);
     if (index % 2 !== 0) {
       result += element * 3;
     } else {
       result += element;
     }
   }
-  return 10 - (result % 10) === Number(c[c.length - 1]);
+  return 10 - (result % 10) === Number(digits[digits.length - 1]);
 };
 
 module.exports = { validateIsbn };
