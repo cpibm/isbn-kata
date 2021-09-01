e8e6fb9534fe19d5d9efb9bf4435a1f2006a9fc5
Carlos Priego <carlos.priego@ibm.com>
2021-09-01 08:26:50 -0500

    test(🔴): 1 passed, 1 failed, 0 skipped

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -7,4 +7,8 @@ describe('It validate the ISBN-13 codes', () => {
     const isbn = '9780470059029';
     expect(validateIsbn(isbn)).toBe(true);
   });
+
+  it('given a numeric input when passed to the validation then we get an invalid input error', () => {
+    expect(() => validateIsbn(9780470059029)).toThrow();
+  });
 });
